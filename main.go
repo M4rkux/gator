5b30e205d50a25c760600280f562c47a6356b6d1
Marcus Amaral Rolim <m4rkux@gmail.com>
2025-06-29 14:45:19 -0300

    add rssFetch

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"context"
 	"database/sql"
+	"encoding/xml"
 	"errors"
 	"fmt"
+	"html"
+	"io"
+	"net/http"
 	"os"
 	"strings"
 	"time"
@@ -23,6 +27,22 @@ type state struct {
 type command struct {
 	name string
 	args []string
+}
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
 }
 
 type commands map[string]func(*state, command) error
@@ -46,6 +66,7 @@ func main() {
 	commands.register("register", handlerRegister)
 	commands.register("reset", handlerReset)
 	commands.register("users", handlerListUsers)
+	commands.register("agg", handlerAgg)
 
 	(*st).cfg, err = config.Read()
 	if err != nil {
@@ -163,6 +184,46 @@ func handlerListUsers(s *state, cmd command) error {
 	}
 
 	return nil
+}
+
+func handlerAgg(_ *state, _ command) error {
+	rssFeed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	if err != nil {
+		return err
+	}
+
+	(*rssFeed).Channel.Title = html.UnescapeString((*rssFeed).Channel.Title)
+	fmt.Println((*rssFeed))
+	return nil
+}
+
+func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, feedURL, nil)
+	if err != nil {
+		return &RSSFeed{}, err
+	}
+	client := &http.Client{}
+
+	req.Header.Set("User-Agent", "gator")
+	resp, err := client.Do(req)
+	if err != nil {
+		return &RSSFeed{}, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return &RSSFeed{}, err
+	}
+
+	rssFeed := &RSSFeed{}
+
+	err = xml.Unmarshal(body, &rssFeed)
+	if err != nil {
+		return &RSSFeed{}, err
+	}
+
+	return rssFeed, nil
 }
 
 func cleanCommandInput(text string) string {
